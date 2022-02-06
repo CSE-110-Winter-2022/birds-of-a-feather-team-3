a46d292dcb677a939d020e3fdadb10bd994be82b
unknown <wanzhangwen@gmail.com>
2022-02-05 23:50:01 -0800

    test file added

---
+++ app/src/test/java/com/example/inputclasses/ViewPersonsListTest.java
@@ -0,0 +1,28 @@
+package com.example.inputclasses;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPersonsListTest {
+
+    @Test
+    public void test1() {
+//        Person Rodney = new Person("Rodney", new String[]{"CSE21","MATH18"});
+//
+//
+//        Person Lucas = new Person("Lucas", new String[]{"ECE45","ECE35","CSE21"});
+//        Person Grace = new Person("Grace", new String[]{"ECE45","ECE35","MATH18"});
+//        Person Mark = new Person("Mark", new String[]{"CSE21","MATH18","WCWP10A"});
+//        Person Vicky = new Person("Vicky", new String[]{"WCWP10B","ECON109","WCWP10A"});
+//
+//        List<Person> fakedata = new ArrayList<>();
+//        fakedata.add(Lucas);
+//        fakedata.add(Grace);
+//        fakedata.add(Mark);
+//        fakedata.add(Vicky);
+//
+//          = SearchClassmates.search(fakedata,Rodney);
+    }
+}
