81da7c645eda652381edcc75f400dc6dd0cbf926
Mark <mkemper@ucsd.edu>
2022-02-12 16:20:25 -0800

    changed xml for larger text

---
+++ app/src/main/java/com/example/inputclasses/ImageLinkEntry.java
@@ -0,0 +1,14 @@
+package com.example.inputclasses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageLinkEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_link_entry);
+    }
+}
