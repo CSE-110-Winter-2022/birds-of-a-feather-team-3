8a50eac228f7aa09bfd81869dbe13bd2abe68327
Mark <mkemper@ucsd.edu>
2022-02-13 17:53:15 -0800

    Preserve State

--- app/src/main/java/com/example/BirdsOfFeather/MockInputPeople.java
+++ app/src/main/java/com/example/BirdsOfFeather/MockInputPeople.java
@@ -22,17 +22,19 @@ public class MockInputPeople extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_mock_nearby_message);
+        String sample = "Bill,,,\n" +
+                "https://lh3.googleusercontent.com/pw/AM-JKLXQ2ix4dg-PzLrPOSMOOy6M3PSUrijov9jCLXs4IGSTwN73B4kr-F6Nti_4KsiUU8LzDSGPSWNKnFdKIPqCQ2dFTRbARsW76pevHPBzc51nceZDZrMPmDfAYyI4XNOnPrZarGlLLUZW9wal6j-z9uA6WQ=w854-h924-no?authuser=0,,,\n" +
+                "2022,WI,CSE,110";
+        TextView inputTextView = (TextView)findViewById(R.id.mockDataTextView);
+        inputTextView.setText(sample);
     }
 
-    public void onEnterClicked(View v){
+
+    public void onMockEnterClicked(View v){
 
         classes = new ArrayList<Course>();
-
-
-
         TextView inputTextView = (TextView)findViewById(R.id.mockDataTextView);
         String inputData = inputTextView.getText().toString();
-
         //split text in textbox at newline character into string array
 
         //copying and pasting example data results in two line breaks per line
@@ -56,16 +58,15 @@ public class MockInputPeople extends AppCompatActivity {
             String number = splitCourse[3];
 
             newCourse = new Course(quarter,year,subject,number);
-
             classes.add(newCourse);
-
         }
 
         newStudent = new Person(name,profileURL,classes);
         System.out.println(newStudent.toString());
 
-        Intent intent = new Intent(this, ViewPersonsList.class);
-        startActivity(intent);
+        finish();
+        //Intent intent = new Intent(this, ViewPersonsList.class);
+        //startActivity(intent);
 
     }
 
--- app/src/main/java/com/example/BirdsOfFeather/ViewPersonsList.java
+++ app/src/main/java/com/example/BirdsOfFeather/ViewPersonsList.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.BirdsOfFeather.database.AppDatabase;
@@ -48,8 +50,21 @@ public class ViewPersonsList extends AppCompatActivity {
     PersonSerializer personSerializer;
     AppDatabase db;
 
+    public void onMockClicked(View v) {
+        Intent intent = new Intent(this, MockInputPeople.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        System.out.println("RESUMED");
+        System.out.println(myCourses.toString());
+        super.onResume();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("CREATED FOR FIRST TIME");
         personSerializer = new PersonSerializer();
         List<String> names;
 //        // fake data of my classes
