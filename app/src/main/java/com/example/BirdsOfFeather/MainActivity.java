fd8eae6a9d326adc8920c210d44ff78a217b8865
Vicki Liang <viliang@ucsd.edu>
2022-02-13 22:06:41 -0800

    bluetooth reminder big

--- app/src/main/java/com/example/BirdsOfFeather/MainActivity.java
+++ app/src/main/java/com/example/BirdsOfFeather/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,8 +13,13 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVERABLE_BT = 0;
+    final BluetoothAdapter btAdapter = BluetoothAdapter.
+            getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
         //If its not their first use, skip inputting name/link
         EditText textView = findViewById(R.id.enter_name_view);
         textView.setText(getNameFromGoogle());
+
+ /*bluetooth reminder, TODO does not work unless forcefully stop app
+        if (!btAdapter.isEnabled()) {
+            Context context = getApplicationContext();
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            Toast toast = Toast.makeText(context, "Please Turn on Bluetooth, Toast.LENGTH_LONG);
+            toast.show();
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        if (!btAdapter.isDiscovering()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            startActivityForResult(enableBtIntent, REQUEST_DISCOVERABLE_BT);
+        }
+*/
+
 
 //        if (!sharedPreferences.getString("first_name", "").equals("")) {
 //            Intent intent = new Intent(this, ViewPersonsList.class);
