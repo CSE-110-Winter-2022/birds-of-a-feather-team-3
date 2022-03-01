cf052fd3c9c114b46648a4f15d3220a9a1a1cfea
Rodney <huangyijie1126@hotmail.com>
2022-03-01 15:24:52 +0800

    Filter score calculation for recent, and testfile added

--- app/src/main/java/com/example/BirdsOfFeather/FliterScoreCalculation.java
+++ app/src/main/java/com/example/BirdsOfFeather/FliterScoreCalculation.java
@@ -1,2 +1,51 @@
-package com.example.BirdsOfFeather;public class FliterScoreCalculation {
+package com.example.BirdsOfFeather;
+
+import java.util.List;
+
+//flitter score calculation class for different type of flitters
+
+//                Course course1 = new Course("Winter", "2020", "CSE", "101");
+//                Course course2 = new Course("Spring", "2020", "CSE", "101");
+//                Course course3 = new Course("Summer Session I", "2020", "CSE", "101");
+//                Course course4 = new Course("Summer Session II", "2020", "CSE", "101");
+//                Course course5 = new Course("Special Summer Session", "2020", "CSE", "101");
+//                Course course6 = new Course("Fall", "2020", "CSE", "101");
+//                Course course7 = new Course("Fall", "2021", "CSE", "101");
+//                Course course8 = new Course("Winter", "2022", "CSE", "101");
+//                Course course9 = new Course("Summer Session I", "2022", "CSE", "101");
+
+public class FliterScoreCalculation {
+
+    //claim this year's information
+    String thisyear = "2022";
+    //String thisquarter = "Winter";
+
+    //calculate score for one person using recent
+    public int score_recent(Person person, Person self){
+
+
+        int finalscore = 0;
+        ProfileInfo profileInfo = SearchClassmates.detectAndReturnSharedClasses(self, person);
+        List<Course> commonCourses = profileInfo.getCommonCourses();
+        for (Course c:commonCourses){
+            String year = c.getYear();
+            String quarter = c.getQuarter();
+            int yeardiff = 4*(Integer.parseInt(thisyear) - Integer.parseInt(year));
+            int quardiff = 0;
+            if (quarter == "Winter"){quardiff = 0;}
+            if (quarter == "Fall"){quardiff = -3;}
+            if (quarter == "Summer Session I" || quarter == "Summer Session II" ||quarter =="Special Summer Session"){
+                quardiff = -2;
+            }
+            if (quarter == "Spring"){quardiff = -1;}
+
+            finalscore = finalscore + yeardiff + quardiff;
+
+
+
+        }
+
+        return finalscore;
+
+    }
 }
---
+++ app/src/test/java/com/example/BirdsOfFeather/FlitterScoreCalculationTest.java
@@ -0,0 +1,63 @@
+package com.example.BirdsOfFeather;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class FlitterScoreCalculationTest {
+    @Test
+    public void properMatchAmount() {
+        List<Person> persons = new ArrayList<>();
+        PersonsViewAdapter personViewAdapter = new PersonsViewAdapter(persons);
+        Course course1 = new Course("Spring", "2020", "CSE", "110");
+        Course course2 = new Course("Fall", "2020", "CSE", "100");
+        Course course3 = new Course("Winter", "2020", "CSE", "101");
+        Course course4 = new Course("Fall", "2020", "WCWP", "10A");
+        List<Course> RodneyClasses = new ArrayList<>(Arrays.asList(course1, course2));
+        List<Course> LucasClasses = new ArrayList<>(Arrays.asList(course4));
+        List<Course> GraceClasses = new ArrayList<>(Arrays.asList(course1, course2, course3));
+        List<Course> MarkClasses = new ArrayList<>(Arrays.asList(course3, course2));
+        List<Course> VickiClasses = new ArrayList<>(Arrays.asList(course1, course4));
+        List<Course> DupVickiClasses = new ArrayList<>(Arrays.asList(course1, course2, course3));
+
+        /***commonalities
+         * Course1: Rodney, Grace, Vicki, DupVicki
+         * Course2: Rodney, Grace, Mark, DupVicki
+         * Course3: Mark
+         * Course4: Vicki, Lucas
+         */
+
+        Person Rodney = new Person("Rodney", "", RodneyClasses);
+        Person Lucas = new Person("Lucas", "", LucasClasses);
+        Person Grace = new Person("Grace", "", GraceClasses);
+        Person Mark = new Person("Mark", "", MarkClasses);
+        Person Vicki = new Person("Vicki", "", VickiClasses);
+        Person DupVicki = new Person("Vicki", "", DupVickiClasses);
+        List<Course> selfClasses = new ArrayList<>(Arrays.asList(course1, course2, course3, course4));
+
+        Person self = new Person("Self", "", selfClasses);
+
+        ProfileInfo RodneyInfo = SearchClassmates.detectAndReturnSharedClasses(self, Rodney);
+        ProfileInfo LucasInfo = SearchClassmates.detectAndReturnSharedClasses(self, Lucas);
+        ProfileInfo GraceInfo = SearchClassmates.detectAndReturnSharedClasses(self, Grace);
+        ProfileInfo MarkInfo = SearchClassmates.detectAndReturnSharedClasses(self, Mark);
+        ProfileInfo VickiInfo = SearchClassmates.detectAndReturnSharedClasses(self, Vicki);
+        ProfileInfo DupVickiInfo = SearchClassmates.detectAndReturnSharedClasses(self, DupVicki);
+
+        assertEquals(2, RodneyInfo.getCommonCourses().size());
+        assertEquals(1, LucasInfo.getCommonCourses().size());
+        assertEquals(3, GraceInfo.getCommonCourses().size());
+        assertEquals(2, MarkInfo.getCommonCourses().size());
+        assertEquals(2, VickiInfo.getCommonCourses().size());
+        assertEquals(3, DupVickiInfo.getCommonCourses().size());
+
+        FliterScoreCalculation fliterScoreCalculation = new FliterScoreCalculation();
+        assertEquals(12,fliterScoreCalculation.score_recent(Rodney,self));
+        assertEquals(5,fliterScoreCalculation.score_recent(Lucas,self));
+        assertEquals(13,fliterScoreCalculation.score_recent(Mark,self));
+    }
+}
